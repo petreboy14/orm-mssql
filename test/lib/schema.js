74add33218450d1a8340c15d3d683d9f15f8dd1b
Peter Henning <petreboy14@gmail.com>
2014-04-13 22:29:42 -0700

    Add base schema class

---
+++ test/lib/schema.js
@@ -0,0 +1,30 @@
+var Joi = require('joi');
+var Lab = require('lab');
+var should = require('should');
+
+var describe = Lab.experiment;
+var it = Lab.test;
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+
+var Schema = require('../../lib/schema');
+
+describe('Schema tests', function () {
+  it('should have a valid TYPES object', function (done) {
+    should.exist(Schema.Types);
+    Schema.Types.should.be.an.instanceOf(Object);
+    done();
+  });
+
+  it('should be able to create a valid schema without error', function (done) {
+    var schema = new Schema({
+      name: {
+        type: Schema.Types.NVarChar,
+        min: 2,
+        max: 50
+      }
+    });
+    done();
+  });
+});
